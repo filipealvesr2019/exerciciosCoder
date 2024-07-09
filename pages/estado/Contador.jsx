5e36a476b7b13bda8118b0fb603d14de1e1f2e09
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-09 17:32:16 -0300

    execicio:  Comunicação Direta

---
+++ pages/estado/Contador.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export default function Contador(){
+    const [contador, setContador] = useState(0)
+    const haldleClickAdd = () => {
+        setContador(contador => contador + 1)
+    }
+    const haldleClickRemove = () => {
+        setContador(contador => contador - 1)
+    }
+    return (
+        <div style={{
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center", 
+            justifyContent:"center",
+            gap:"2rem"
+        }}>
+        <div  style={{
+            display:"flex",
+            alignItems:"center", 
+         
+            gap:"2rem"
+        }}>
+            
+        <span onClick={haldleClickRemove}>-</span>
+        <h1>Contador</h1>
+        <span onClick={haldleClickAdd}>+</span>
+
+        </div>
+        <span>{contador}</span>
+        </div>
+    )
+}
