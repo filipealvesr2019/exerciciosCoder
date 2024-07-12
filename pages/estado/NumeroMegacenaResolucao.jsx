ca64548f66cacec6aad44ac3fa0971a23605dfe4
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-12 15:53:54 -0300

    coder Nextjs: desafio mega sena

---
+++ pages/estado/NumeroMegacenaResolucao.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export default function NumeroMegacenaResolucao(){
+    const [gerarNumeros, setGerarNumeros] = useState([])
+    const gerarNumerosClick = () =>{
+        let numerosGerados = new Set();
+        while (numerosGerados.size < 6){
+            let numero = Math.floor(Math.random() * 60) + 1 ;
+            numerosGerados.add(numero);
+
+        }
+        setGerarNumeros(Array.from(numerosGerados))
+    }
+    return (
+        <>
+        <div style={{
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center", 
+            justifyContent:"center",
+            gap:"2rem"
+        }}>
+       
+        <span>{gerarNumeros}</span>
+        
+        <button onClick={gerarNumerosClick}>gerar numeros</button>
+        </div>
+        
+        
+        </>
+    )
+}
