2532b36a4a775395e3cf0698613727a4353f30a2
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-15 16:53:16 -0300

    Desafio Mega-Sena - Resposta #01

---
+++ functions/mega.js
@@ -0,0 +1,17 @@
+export function mega(quantidade = 6, numeros = []){
+if(quantidade < 6 && quantidade > 60){
+    throw 'quantidade invalida'
+}
+if (numeros.length === quantidade){
+    return numeros.sort((n1, n2 ) => n1 - n2)
+}
+
+const numeroAleatorio = parseInt(Math.random() * 60) + 1
+if(!numeros.includes(numeroAleatorio)){
+return mega(quantidade, [...numeros, numeroAleatorio])
+} else{
+    return mega(quantidade, numeros)
+
+}
+}
+console.log(mega())
