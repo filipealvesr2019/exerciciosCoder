e8801aac68cad116673063475487895bb467e2aa
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-23 17:17:23 -0300

    coder nextJS: Sistema Módulo ECMAScript

---
+++ components/modulo/Funcionais.jsx
@@ -0,0 +1,30 @@
+export function componente1(){
+    return <h2>componente1</h2>
+}
+
+export const componente2 = function(){
+    return <h2>componente2</h2>
+}
+
+export default function componente3(){
+    return <h2>componente3</h2>
+}
+
+// export const componente4 = props =>  <h2>componente4</h2>
+
+export const componente4 = () => <h2>componente4</h2>
+
+export const componente5 = () => <h2>componente5</h2>
+
+export const componente6  = () => (
+    <div>
+         <h2>componente6</h2>
+    </div>
+)
+
+// export default () => (
+//     <div>
+//          <h2>componente6</h2>
+//     </div>
+// )
+
---
+++ pages/modulo/Teste.jsx
@@ -0,0 +1,15 @@
+import Padrao, { componente1 as Componente1, componente2 as Componente2, componente4 as Componente4, componente5 as Componente5,  componente6 as Componente6, } from "../../components/modulo/Funcionais";
+5
+export default function teste(){
+    return (
+        <div>
+            <Componente1 />
+            <Componente2 />
+            <Padrao />
+            <Componente4 />
+            <Componente5 />
+            <Componente6 />
+
+        </div>
+    )
+}
